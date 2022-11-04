26ad8176e3d189c48ca28848880d8ca0802d23eb
Nadhi Woliye <nadhiwoliye@Nadhis-MacBook-Air.local>
2022-11-04 17:09:43 -0500

    create the method for the present with question

--- src/ArtistManager.java
+++ src/ArtistManager.java
@@ -1,3 +1,24 @@
-public class ArtistManager implements java.io.Serializable{
+import java.util.HashMap;
+import java.util.Scanner;
 
+public class ArtistManager {
+        HashMap<Integer,Artist> artists;
+        Scanner scan = new Scanner(System.in);
+    public ArtistManager() {
+        this.artists = new HashMap<Integer,Artist>();
+    }
+
+    public void present(){
+        System.out.println("What is the name of the artist?");
+        String name = scan.nextLine();
+        System.out.println("What your favorite song?");
+        String song = scan.nextLine();
+        System.out.println("What year did song came out?");
+        Integer year = Integer.parseInt(scan.nextLine());
+        System.out.println("What is the id?");
+        Integer id = Integer.parseInt(scan.nextLine());
+
+        Artist songs = new Artist(name,song,year);
+        artists.put(id,songs);
+    }
 }
