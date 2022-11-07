6576f0391f850ca6e729a95cc12cd8581be7e792
Nadhi Woliye <nadhiwoliye@Nadhis-MacBook-Air.local>
2022-11-06 21:38:44 -0600

    crate the main

--- src/ArtistManager.java
+++ src/ArtistManager.java
@@ -1,3 +1,6 @@
+import javax.imageio.IIOException;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -8,7 +11,8 @@ public class ArtistManager {
         this.artists = new HashMap<Integer,Artist>();
     }
 
-    public void present(){
+//
+    public void create(){
         System.out.println("What is the name of the artist?");
         String name = scan.nextLine();
         System.out.println("What your favorite song?");
@@ -21,6 +25,8 @@ public class ArtistManager {
         Artist songs = new Artist(name,song,year);
         artists.put(id,songs);
         print();
+        serializeArtist(artists);
+        deserializeArtist();
     }
 
     public void read(){
@@ -54,9 +60,61 @@ public class ArtistManager {
         print();
     }
 
+    public static void serializeArtist(HashMap<Integer,Artist> artist){
+        try{
+            FileOutputStream fileOut = new FileOutputStream("employees.ser");
+            // ^ opening a connect to a new file and allowing to connect
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            // ^ streaming data from an object into a file
+            out.writeObject(artist);
+            // take this object and i'm lobbing it
+            out.close();
+            // close it once we are done with the file
+            fileOut.close();
+            // close it once we are done with the file
+            System.out.println("Serialized data is saved!");
+
+
+        }catch (IOException i) {
+            i.printStackTrace();
+            // history of all the methods that were called - allows us to see where the code went wrong.
+            //principle of a stack is similar to pringles - last in, first out
+        }
+
+    }
+
+    public static void deserializeArtist() {
+// we need to read from the file object.ser the data for our employee
+// and if possible create a new employee otherwise return null
+
+        HashMap<Integer,Artist> artist = new HashMap<Integer,Artist>(); // this create an object of type employee to receive data from file or return
+
+        try {
+            // read object from a file
+            FileInputStream file = new FileInputStream("employees.ser");
+            // create a connect to a file
+            ObjectInputStream in = new ObjectInputStream(file);
+
+            // method for deserialization for an object
+            artist = (HashMap<Integer, Artist>) in.readObject();
+            // ^ read object and convert data to type Employee
+
+            in.close();
+            file.close();
+
+            System.out.println("Object has been deserialized");
+            System.out.println(artist.size());
+
+        } catch (IOException i) {
+            i.printStackTrace();
+
+        } catch (ClassNotFoundException c) {
+            c.printStackTrace();
+        }
+    }
     public void print(){
         for (Integer key:artists.keySet()) {
-            System.out.println("Id is: " + key +" " + artists.keySet());
+            System.out.println("Id is: " + key +" " + artists.get(key));
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
+//    static ArtistManager artists = new ArtistManager();
+
+    static ArtistManager artists = new ArtistManager();
+    static Scanner scan = new Scanner(System.in);
+
+    static String option = " ";
+public static void present(){
+    System.out.println("Type C to create your playlist\n" + "Type R to see all playlist" + "Type U to update playlist\n" + "Type D to Delete a playlist\n" + "Type Q to quiz");
+    option = scan.nextLine();
+    if (option.equalsIgnoreCase("C")) {
+        artists.create();
+    } else if (option.equalsIgnoreCase("R")) {
+        artists.read();
+    } else if (option.equalsIgnoreCase("U")) {
+        artists.update();
+    } else if (option.equalsIgnoreCase("D")) {
+        artists.remove();
+    } else if (option.equalsIgnoreCase("Q")) {
+        System.out.println("Goodbye");
+    }
+}
+    public static void main(String[] args) throws Exception{
+
+//        ArtistManager artists = new ArtistManager();
+//        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Welcome to Oromo Music");
+        while(!option.equalsIgnoreCase("Q")) {
+            present();
+        }
 
     }
 }
